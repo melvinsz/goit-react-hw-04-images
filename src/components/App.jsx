029cea0e515fca8a8b5a0a32398176bbf4a90eb3
Stanislav <Stas.zaliiski@gmail.com>
2023-05-14 15:26:52 +0200

    main

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { ColorRing } from 'react-loader-spinner';
 
 import Searchbar from './Searchbar/Searchbar';
@@ -8,97 +8,182 @@ import Modal from './Modal/Modal';
 
 const API_KEY = '34746416-8804c3e057cfbf229fa5fe7fd';
 
-class App extends Component {
-  state = {
-    value: '',
-    images: [],
-    page: 1,
-    loading: false,
-    showModal: false,
-    largeImageURL: '',
-    alt: '',
+const App = () => {
+  const [value, setValue] = useState('');
+  const [images, setImage] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [largeImageURL, setLargeImageURL] = useState('');
+  const [alt, setAlt] = useState('');
+
+  const handleChange = event => {
+    setValue(event.target.value);
+    setPage(1);
   };
 
-  handleChange = event => {
-    this.setState({ value: event.target.value, page: 1 });
-  };
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    if (this.state.value !== '') {
-      this.setState({ loading: true });
+    if (value !== '') {
+      setLoading(true);
       fetch(
-        `https://pixabay.com/api/?q=${this.state.value}&page=${this.state.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+        `https://pixabay.com/api/?q=${value}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
       )
         .then(response => response.json())
         .then(data => {
-          this.setState({ images: data.hits, loading: false });
+          setImage(data.hits);
+          setLoading(false);
         })
         .catch(error => {
           console.log(error.message);
-          this.setState({ loading: false });
+          setLoading(false);
         });
     } else return;
   };
 
-  handleClick = event => {
+  const handleClick = event => {
     event.preventDefault();
-    if (this.state.images.length > 0) {
-      this.setState({ loading: true });
+    if (images.length > 0) {
+      setLoading(true);
       fetch(
-        `https://pixabay.com/api/?q=${this.state.value}&page=${
-          this.state.page + 1
+        `https://pixabay.com/api/?q=${value}&page=${
+          page + 1
         }&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
       )
         .then(response => response.json())
         .then(data => {
-          this.setState(prevState => ({
-            images: [...prevState.images, ...data.hits],
-            page: prevState.page + 1,
-            loading: false,
-          }));
+          setImage(prevState => [...prevState.images, ...data.hits]);
+          setPage(prevState => prevState + 1);
+          setLoading(false);
         })
         .catch(error => {
           console.log(error.message);
-          this.setState({ loading: false });
+          setLoading(false);
         });
     } else return;
   };
 
-  handleOpenModal = (largeImageURL, alt) => {
-    this.setState({
-      showModal: true,
-      largeImageURL: largeImageURL,
-      alt: alt,
-    });
+  const handleOpenModal = (largeImageURL, alt) => {
+    setShowModal(true);
+    setLargeImageURL(largeImageURL);
+    setAlt(alt);
   };
 
-  handleCloseModal = () => {
-    this.setState({ showModal: false });
+  const handleCloseModal = () => {
+    setShowModal(false);
   };
 
-  render() {
-    const { loading, images, showModal, largeImageURL, alt } = this.state;
+  return (
+    <div className="app">
+      <Searchbar onSubmit={handleSubmit} onChange={handleChange} />
+      {loading && (
+        <div className="loader">
+          <ColorRing />
+        </div>
+      )}
+      <ImageGallery images={images} onClickImage={handleOpenModal} />
+      {images.length > 0 && <Button onClick={handleClick} />}
+      <Modal
+        showModal={showModal}
+        onClose={handleCloseModal}
+        src={largeImageURL}
+        alt={alt}
+      />
+    </div>
+  );
+};
 
-    return (
-      <div className="app">
-        <Searchbar onSubmit={this.handleSubmit} onChange={this.handleChange} />
-        {loading && (
-          <div className="loader">
-            <ColorRing />
-          </div>
-        )}
-        <ImageGallery images={images} onClickImage={this.handleOpenModal} />
-        {images.length > 0 && <Button onClick={this.handleClick} />}
-        <Modal
-          showModal={showModal}
-          onClose={this.handleCloseModal}
-          src={largeImageURL}
-          alt={alt}
-        />
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   state = {
+//     value: '',
+//     images: [],
+//     page: 1,
+//     loading: false,
+//     showModal: false,
+//     largeImageURL: '',
+//     alt: '',
+//   };
+
+//   handleChange = event => {
+//     this.setState({ value: event.target.value, page: 1 });
+//   };
+
+//   handleSubmit = event => {
+//     event.preventDefault();
+//     if (this.state.value !== '') {
+//       this.setState({ loading: true });
+//       fetch(
+//         `https://pixabay.com/api/?q=${this.state.value}&page=${this.state.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+//       )
+//         .then(response => response.json())
+//         .then(data => {
+//           this.setState({ images: data.hits, loading: false });
+//         })
+//         .catch(error => {
+//           console.log(error.message);
+//           this.setState({ loading: false });
+//         });
+//     } else return;
+//   };
+
+//   handleClick = event => {
+//     event.preventDefault();
+//     if (this.state.images.length > 0) {
+//       this.setState({ loading: true });
+//       fetch(
+//         `https://pixabay.com/api/?q=${this.state.value}&page=${
+//           this.state.page + 1
+//         }&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+//       )
+//         .then(response => response.json())
+//         .then(data => {
+//           this.setState(prevState => ({
+//             images: [...prevState.images, ...data.hits],
+//             page: prevState.page + 1,
+//             loading: false,
+//           }));
+//         })
+//         .catch(error => {
+//           console.log(error.message);
+//           this.setState({ loading: false });
+//         });
+//     } else return;
+//   };
+
+//   handleOpenModal = (largeImageURL, alt) => {
+//     this.setState({
+//       showModal: true,
+//       largeImageURL: largeImageURL,
+//       alt: alt,
+//     });
+//   };
+
+//   handleCloseModal = () => {
+//     this.setState({ showModal: false });
+//   };
+
+//   render() {
+//     const { loading, images, showModal, largeImageURL, alt } = this.state;
+
+//     return (
+//       <div className="app">
+//         <Searchbar onSubmit={this.handleSubmit} onChange={this.handleChange} />
+//         {loading && (
+//           <div className="loader">
+//             <ColorRing />
+//           </div>
+//         )}
+//         <ImageGallery images={images} onClickImage={this.handleOpenModal} />
+//         {images.length > 0 && <Button onClick={this.handleClick} />}
+//         <Modal
+//           showModal={showModal}
+//           onClose={this.handleCloseModal}
+//           src={largeImageURL}
+//           alt={alt}
+//         />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,34 +1,47 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+const Searchbar = ({ onSubmit, onChange }) => {
+  return (
+    <header className="searchbar">
+      <form className="form" onSubmit={onSubmit}>
+        <button type="submit" className="button">
+          <span className="button-label">Search</span>
+        </button>
 
-class Searchbar extends Component {
-  render() {
-    const { onSubmit, onChange } = this.props;
-
-    return (
-      <header className="searchbar">
-        <form className="form" onSubmit={onSubmit}>
-          <button type="submit" className="button">
-            <span className="button-label">Search</span>
-          </button>
-
-          <input
-            className="input"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            onChange={onChange}
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          className="input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          onChange={onChange}
+        />
+      </form>
+    </header>
+  );
+};
 
 export default Searchbar;
 
-Searchbar.propTypes = {
-  onSubmit: PropTypes.func,
-  onChange: PropTypes.func,
-};
+// class Searchbar extends Component {
+//   render() {
+//     const { onSubmit, onChange } = this.props;
+
+//     return (
+//       <header className="searchbar">
+//         <form className="form" onSubmit={onSubmit}>
+//           <button type="submit" className="button">
+//             <span className="button-label">Search</span>
+//           </button>
+
+//           <input
+//             className="input"
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//             onChange={onChange}
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
